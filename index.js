d2df186dc8de039189a7ca44993602e12b3d49f0
MaximilianMcC <max@maximilian.co.nz>
2024-08-16 11:28:07 +1200

    Fancy 404 page

--- index.js
+++ index.js
@@ -10,13 +10,19 @@ const port = process.env.PORT || 3000;
 // stuff (normal plain html website thing)
 app.use(Express.static(Path.join(__dirname, "public")));
 
+// Set up all the routers so we can use stuff
+// from other files (keeping clean fr)
+app.get("/test", (request, response) => {
+	response.send("<h1>testing rn</p>");
+})
+
 // Make it so that all pages don't have
 // the .html part at the end of them
-app.get("/:page", (request, response) => {
+app.get("*", (request, response) => {
 
-	// Get the page we're after
-	const pageName = request.params.page;
-	const page = Path.join(__dirname, "public", `${pageName}.html`);
+	// Get the stuff we're after
+	const requestedContent = request.params[0];
+	const page = Path.join(__dirname, "public", `${requestedContent}.html`);
 
 	// Check for if the page exists. If it does then
 	// send them the page. Otherwise pack a sad
@@ -27,14 +33,25 @@ app.get("/:page", (request, response) => {
 	}
 
 	// dodgy error screen
-	response.send(`<h1>404 or something</h1><p>can't find whatever <i>${pageName}</i> is sorry (maybe you've got the dodgy spelling)</p>`);
+	Send404(requestedContent, response);
 });
-
-// Set up all the routers so we can use stuff
-// from other files (keeping clean fr)
-app.get("/test", (request, response) => {
-	response.send("<h1>testing rn</p>");
-})
 
 // Run the express server
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
+
+
+
+// Send a fancy as 404 page
+function Send404(missingThing, response) {
+	
+	// Get the 404 html
+	const path = Path.join(__dirname, "public", "responses", "404.html");
+	let html = FileSystem.readFileSync(path, "utf8");
+
+	// Replace the error text thing
+	html = html.replace("{missingThing}", missingThing);
+
+	// Send back the 404 file and whatnot with
+	// the 404 status code also
+	response.status(404).send(html);
+}
