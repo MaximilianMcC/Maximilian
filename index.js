e40119c217dd0ab195d264e5c0b09b4ef6616126
MaximilianMcC <max@maximilian.co.nz>
2024-08-17 14:21:55 +1200

    speedrun and error rewrites

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const Express = require("express");
 const Path = require("path");
 const FileSystem = require("fs");
+const Utils = require("./utils");
 
 // Setup express
 const app = Express();
@@ -10,11 +11,12 @@ const port = process.env.PORT || 3000;
 // stuff (normal plain html website thing)
 app.use(Express.static(Path.join(__dirname, "public")));
 
+// Router endpoints (stuff from other files)
+app.use("/apis/speedrun", require("./apis/speedrun/speedrun"));
+
 // Endpoints and whatnot
 app.get("/test", (request, response) => {
-	// response.send("<h1>testing rn</h1>");
-
-	SendCustomError("test", 403, response);
+	response.send("<h1>testing rn</h1>");
 })
 
 // Make it so that all pages don't have
@@ -28,25 +30,8 @@ app.get("*", (request, response) => {
 	// Check for if the page exists. If it does then
 	// send them the page. Otherwise pack a sad
 	if (FileSystem.existsSync(page)) response.sendFile(page);
-	else SendCustomError(requestedContent, 404, response);
+	else Utils.SendCustomError(requestedContent, 404, response);
 });
 
 // Run the express server
 app.listen(port, () => console.log(`Server listening on port ${port}!`));
-
-
-
-// Send a fancy as error page
-function SendCustomError(statusText, status, response) {
-	
-	// Get the errors html page as a string
-	const path = Path.join(__dirname, "public", "responses", `${status}.html`);
-	let html = FileSystem.readFileSync(path, "utf8");
-
-	// Replace the error text thing
-	html = html.replace("{statusText}", statusText);
-
-	// Send back the file and whatnot with
-	// the status code also
-	response.status(status).send(html);
-}
