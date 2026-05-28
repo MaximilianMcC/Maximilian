70f0790808f854446da54e4a62b5c073d324cf91
MaximilianMcC <max@maximilian.co.nz>
2026-05-28 21:38:27 +1200

    Fixed null values + started working on something which I forgot

--- public/apis/picmix/picmix.js
+++ public/apis/picmix/picmix.js
@@ -6,6 +6,9 @@ const Utils = require("../../../utils");
 const picmixBaseUrl = "https://www.picmix.com";
 const headers = {
 	"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120 Safari/537.36",
+	"Accept": "text/html,application/xhtml+xml",
+	"Accept-Language": "en-US,en;q=0.9",
+	"Connection": "keep-alive"
 };
 
 // TODO: Add a 10 minute cache or something
@@ -15,6 +18,7 @@ async function scrapeWebsite(url) {
 		// Get the website
 		const response = await Axios.get(url, {
 			headers,
+			maxRedirects: 5,
 			validateStatus: () => true
 		});
 
@@ -89,6 +93,10 @@ function convertHtmlEmojisToPlainText(paragraph) {
 	return result;
 }
 
+function removeBrackets(string) {
+	return string.replace("(", "").replace(")", "");
+}
+
 function initPicmixApi(app) {
 
 	// Get information about a user (profile)
@@ -104,9 +112,9 @@ function initPicmixApi(app) {
 		if (website.error)
 		{
 			console.log(`couldn't scape (got ${website.httpStatus} from ${url})`);
-
+			
 			if (website.httpStatus === 404) return Utils.SendCustomError(`No user with the name ${requestedUsername}`, 404, response);
-			if (website.httpStatus === 403) return Utils.SendCustomError(`Picmix blocked the request <pre>${result.html.slice(0, 500)}</pre>`, 500, response);
+			if (website.httpStatus === 403) return Utils.SendCustomError(`Picmix blocked the request <pre>${website}</pre>`, 500, response);
 			return Utils.SendCustomError(`Could not scrape picmix for some reason.`, 500, response);
 		}
 
@@ -148,7 +156,11 @@ function initPicmixApi(app) {
 		apiResponse["avatarPicmix"] = convertPicmixToBasicObject(dom.querySelector("div#pAvatar a"));
 
 		// Get the total picmix count (up to 36)
-		apiResponse["totalPicmix"] = Number((dom.querySelector("div#pPics h3.pTitle span").textContent).replace("(", "").replace(")", ""));
+		const picmixElement = dom.querySelector("div#pPics h3.pTitle span")?.textContent;
+		let picmixCount = picmixElement == null ? 0 : Number(removeBrackets(picmixElement));
+		apiResponse["totalPicmix"] = picmixCount;
+
+		// Get all the picmixs
 		{
 			let recents = [];
 			dom.querySelectorAll("div#pPics div.list-grid div.container a").forEach(picmix => {
@@ -158,20 +170,60 @@ function initPicmixApi(app) {
 		}
 
 		// Get the total friends count
-		apiResponse["totalFriends"] = Number((dom.querySelector("div#pFriends h3.pTitle span").textContent).replace("(", "").replace(")", ""));
+		const friendsElement = dom.querySelector("div#pFriends h3.pTitle span")?.textContent;
+		let friends = friendsElement == null ? 0 : Number(removeBrackets(friendsElement));
+		apiResponse["totalFriends"] = friends;
 
 		// Get the total received gifts
-		apiResponse["totalRecievedGifts"] = Number((dom.querySelector("div#pGifts h3.pTitle span").textContent).replace("(", "").replace(")", ""));
+		const giftsElement = dom.querySelector("div#pGifts h3.pTitle span")?.textContent;
+		let gifts = giftsElement == null ? 0 : Number(removeBrackets(giftsElement));
+		apiResponse["totalRecievedGifts"] = gifts;
 
 		// Get the total contest entries
-		apiResponse["totalContestEntries"] = Number((dom.querySelector("div#pContests h3.pTitle span").textContent).replace("(", "").replace(")", ""));
+		const contestsElement = dom.querySelector("div#pContests h3.pTitle span")?.textContent;
+		let contests = contestsElement == null ? 0 : Number(removeBrackets(contestsElement));
+		apiResponse["totalContestEntries"] = contests;
 
 		// Get the total sticker count
 		// TODO: Rename to stamps because that's french or whatever
-		apiResponse["totalStickers"] = Number((dom.querySelector("div#pStamps h3.pTitle span").textContent).replace("(", "").replace(")", ""));
+		const stickersElement = dom.querySelector("div#pStamps h3.pTitle span")?.textContent;
+		let stickers = stickersElement == null ? 0 : Number(removeBrackets(stickersElement));
+		apiResponse["totalStickers"] = stickers;
 
 		// Get the total comments count
-		apiResponse["totalComments"] = Number((dom.querySelector("div#pComments h3.pTitle span").textContent).replace("(", "").replace(")", ""));
+		const commentElement = dom.querySelector("div#p h3.pComments span")?.textContent;
+		let comments = commentElement == null ? 0 : Number(removeBrackets(commentElement));
+		apiResponse["totalComments"] = comments;
+
+		return response.json(apiResponse);
+	});
+
+	// Get information about a user (profile)
+	app.get("/api/picmix/profile", async (request, response) => {
+
+		// Get the users name
+		const requestedPicmixId = request.query.username;
+		if (!requestedPicmixId) return Utils.SendCustomError("Please supply an ?id parameter", 400, response);
+
+		// Get the site
+		const url = `${picmixBaseUrl}/pic/${requestedPicmixId}`;
+		const website = await scrapeWebsite(url);
+		if (website.error)
+		{
+			console.log(`couldn't scape (got ${website.httpStatus} from ${url})`);
+			
+			if (website.httpStatus === 404) return Utils.SendCustomError(`No user with the name ${requestedPicmixId}`, 404, response);
+			if (website.httpStatus === 403) return Utils.SendCustomError(`Picmix blocked the request <pre>${website}</pre>`, 500, response);
+			return Utils.SendCustomError(`Could not scrape picmix for some reason.`, 500, response);
+		}
+
+		const dom = website;
+		if (!dom) return Utils.SendCustomError("Issue on my end whilst tryna scrape the picmix website", 500, response);
+
+		// Place where we're gonna store the data
+		let apiResponse = {}
+
+		
 
 		return response.json(apiResponse);
 	});
