690ca7a053e3af5bc762d5f651586e8c957abea6
MaximilianMcC <max@maximilian.co.nz>
2026-05-30 10:31:29 +1200

    progress bar stuff

---
+++ public/script/portal.js
@@ -0,0 +1,58 @@
+
+// Toggle the icons on/off when we click on them
+document.querySelectorAll(".icons img").forEach(icon => {
+	icon.addEventListener("click", () => {
+
+		// Toggle the on/off state
+		icon.classList.toggle("off");
+	});
+});
+
+const progressText = document.querySelector("#progressText");
+progressText.addEventListener("input", () => {
+
+	// If we have a malformed input then set it to be full
+	if (progressText.value.includes("/") == false) {
+		setProgressBar(1, 1);
+		return;
+	}
+
+	// Parse the text to get the x/y
+	const segments = progressText.value.split("/");
+	const currentChamber = Number(segments[0]);
+	const totalChambers = Number(segments[1]);
+
+	if (!Number.isFinite(currentChamber) || !Number.isFinite(totalChambers)) {
+		setProgressBar(1, 1);
+		return;
+	}
+
+	setProgressBar(currentChamber, totalChambers);
+});
+
+function setProgressBar(currentChamber, maxChambers) {
+	
+	// TODO: Have this defined in the html instead of js
+	const maxProgressBars = 23; 
+	const minProgressBars = 1;
+	const progressBarHtml = `<img src="/image/portal/chamber-progress-bars.png" alt="|">`;
+
+	// Figure out how many progress bars we need
+	const percent = currentChamber / maxChambers;
+	let barsNeeded = clamp(maxProgressBars * percent, minProgressBars, maxProgressBars);
+
+	// Clear however many bars we had before
+	const progressBars = document.querySelector(".progress-bars");
+	progressBars.innerHTML = "";
+
+	// Add our new bars
+	for (let i = 0; i < barsNeeded; i++) {
+		progressBars.innerHTML += progressBarHtml;
+	}
+}
+
+function clamp(value, min, max) {
+	if (value > max) return max;
+	if (value < min) return min;
+	return value;
+}
