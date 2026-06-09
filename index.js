cd3160038340f85136ba49095ce4602f8a274c4e
MaximilianMcC <max@maximilian.co.nz>
2026-06-09 23:28:35 +1200

    began working on book thing

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.use(Express.static(Path.join(__dirname, "assets")));
 app.use("/style", Express.static(Path.join(__dirname, "public", "style")));
 app.use("/script", Express.static(Path.join(__dirname, "public", "script")));
 app.use("/font", Express.static(Path.join(__dirname, "public", "font")));
+app.use("/apis/books/", Express.static(Path.join(__dirname, "dynamic", "books.json")));
 
 // Endpoints and whatnot
 app.get("/test", (request, response) => {
